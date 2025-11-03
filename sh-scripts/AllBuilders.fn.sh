cc5e1fff67d9679a94374e5d8a3021fef824ba4c
Alex Kharichev <myx@myx.nz>
2025-11-03 22:27:55 +0200

    * step builders relocated and support .local

--- sh-scripts/AllBuilders.fn.sh
+++ sh-scripts/AllBuilders.fn.sh
@@ -7,61 +7,57 @@ if [ -z "$MMDAPP" ] ; then
 	[ -d "$MMDAPP/source" ] || ( echo "⛔ ERROR: expecting 'source' directory." >&2 && exit 1 )
 fi
 
-AllActions(){
+AllBuilders(){
 
-	local MDSC_CMD='AllActions'
+	local MDSC_CMD='AllBuilders'
 
 	case "$1" in
-		--completion)
-			if [ full = "$MDSC_DETAIL" ]; then
-				echo "> $MDSC_CMD $@" >&2
-				local MDSC_DETAIL=true
-			else
-				[ -z "$MDSC_DETAIL" ] || local MDSC_DETAIL=
-			fi
-		;;
 		--no-formatting)
 			shift
 		;;
 		*)
 			[ -z "$MDSC_DETAIL" ] || echo "> $MDSC_CMD $(printf '%q ' "$@")" >&2
 			if [ -t 1 ] && command -v column >/dev/null 2>&1; then
-				AllActions --no-formatting "$@" | column -t
+				AllBuilders --no-formatting "$@" | column -t
 				return 0
 			fi
 		;;
 	esac
 
-	local sedEx="sed -e 's:^$MMDAPP/source/::' -e 's:^$MDSC_SOURCE/::' -e 's:^$MDLT_ORIGIN/::' -e 's:/actions/: :'"
+	local sedEx="sed -e 's:^$MMDAPP/source/::' -e 's:^$MDSC_SOURCE/::' -e 's:^$MDLT_ORIGIN/::' -e 's:/builders/: :'"
+	local stageFilter=
 
 	. "$MDLT_ORIGIN/myx/myx.distro-system/sh-lib/SystemContext.UseStandardOptions.include"
 	while [ $# -gt 0 ]; do
 		case "$1" in
-			--completion)
-				sedEx="sed -e 's:^$MMDAPP/source/::' -e 's:^$MDSC_SOURCE/::' -e 's:^$MDLT_ORIGIN/::' -e 's:^.*/actions/::'"
-				shift ; break
-			;;
 			--executables)
 				sedEx="cat"
 				shift
 			;;
 			--scripts)
-				sedEx="sed -e 's:^$MMDAPP/source/::' -e \"s:^$MDSC_SOURCE/::' -e 's:^$MDLT_ORIGIN/::'"
-				shift ;	break
+				sedEx="sed -e 's:^$MMDAPP/source/::' -e 's:^$MDSC_SOURCE/::' -e 's:^$MDLT_ORIGIN/::'"
+				shift
 			;;
 			--full)
-				sedEx="sed -e 's:^$MMDAPP/source/::' -e 's:^$MDSC_SOURCE/::' -e 's:^$MDLT_ORIGIN/::' -e 's:^\(.*\)/actions/:\1 \1/actions/:'"
-				shift ; break
+				sedEx="sed -e 's:^$MMDAPP/source/::' -e 's:^$MDSC_SOURCE/::' -e 's:^$MDLT_ORIGIN/::' -e 's:^\(.*\)/builders/:\1 \1/builders/:'"
+				shift
 			;;
-			--|--default|--all-actions)
-				# sedEx="sed -e 's:^$MMDAPP/source/::' -e 's:^$MDSC_SOURCE/::' -e 's:^$MDLT_ORIGIN/::' -e 's:/actions/: :'"
-				break
+			--|--default|--all-builders)
+				shift
+			;;
+			--all-build-stages)
+				shift ;	break
+			;;
+			source-prepare|source-process|image-prepare|image-process|image-install)
+				stageFilter="$1"
+				sedEx="grep '/builders/$stageFilter/' | $sedEx"
+				shift ;	break
 			;;
 			--help|--help-syntax)
-				echo "📘 syntax: AllActions.fn.sh --default|--full|--scripts|..." >&2
-				echo "📘 syntax: AllActions.fn.sh [--help]" >&2
+				echo "📘 syntax: AllBuilders.fn.sh --default|--full|--scripts| [--all-build-stages]..." >&2
+				echo "📘 syntax: AllBuilders.fn.sh [--help]" >&2
 				if [ "$1" = "--help" ] ; then
-					cat "$MDLT_ORIGIN/myx/myx.distro-system/sh-lib/help/Help.AllActions.text" >&2
+					cat "$MDLT_ORIGIN/myx/myx.distro-system/sh-lib/help/Help.AllBuilders.text" >&2
 				fi
 				return 0
 			;;
@@ -74,28 +70,28 @@ AllActions(){
 
 	if [ "$MDSC_NO_CACHE" != "--no-cache" ] && [ -d "$MDSC_CACHED" ] ; then
 
-		local cacheFile="$MDSC_CACHED/distro-actions.txt"
+		local cacheFile="$MDSC_CACHED/distro-builders.txt"
 		local buildDate="$MDSC_CACHED/build-time-stamp.txt"
 
 		if [ -f "$cacheFile" ] && [ -f "$buildDate" ] && [ ! "$cacheFile" -ot "$buildDate" ] ; then
 			[ -z "$MDSC_DETAIL" ] || echo "| $MDSC_CMD: using cached ($MDSC_OPTION)" >&2
-			eval "$sedEx" "$cacheFile"
+			cat "$cacheFile" | eval "$sedEx" 
 			return 0
 		fi
 
 		[ -z "$MDSC_DETAIL" ] || echo "$MDSC_CMD: caching all actions ($MDSC_OPTION)" >&2
-		. "$MDLT_ORIGIN/myx/myx.distro-source/sh-lib/source-context/ScanSourceActions.include" \
+		. "$MDLT_ORIGIN/myx/myx.distro-source/sh-lib/source-context/ScanSourceBuilders.include" \
 		| tee "$cacheFile.$$.tmp" | eval "$sedEx"
 		mv -f "$cacheFile.$$.tmp" "$cacheFile"
 		return 0
 
 	fi
 	
-	. "$MDLT_ORIGIN/myx/myx.distro-source/sh-lib/source-context/ScanSourceActions.include" | eval "$sedEx"
+	. "$MDLT_ORIGIN/myx/myx.distro-source/sh-lib/source-context/ScanSourceBuilders.include" | eval "$sedEx"
 }
 
 case "$0" in
-	*/sh-scripts/AllActions.fn.sh) 
+	*/sh-scripts/AllBuilders.fn.sh) 
 
 		if [ -z "$MDLT_ORIGIN" ] || ! type DistroSystemContext >/dev/null 2>&1 ; then
 			. "${MDLT_ORIGIN:=$MMDAPP/.local}/myx/myx.distro-system/sh-lib/SystemContext.include"
@@ -103,10 +99,10 @@ case "$0" in
 		fi
 
 		if [ -z "$1" ] || [ "$1" = "--help" ] ; then
-			AllActions "${1:-"--help-syntax"}"
+			AllBuilders "${1:-"--help-syntax"}"
 			exit 1
 		fi
 
-		AllActions "$@"
+		AllBuilders "$@"
 	;;
 esac
